07f3492630dfb1c68fb7689c8609c3a6042b99c9
Felix Thaler <thaler@cscs.ch>
2024-05-28 15:26:51 +0200

    Added compressneighbors.hpp

---
+++ include/cstone/compressneighbors.hpp
@@ -0,0 +1,142 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 CSCS, ETH Zurich
+ *               2021 University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*! @file
+ * @brief Neighbor list compression tests
+ *
+ * @author Felix Thaler <thaler@cscs.ch>
+ */
+
+#include <algorithm>
+#include <bit>
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+
+namespace cstone
+{
+
+class NeighborListCompressor
+{
+public:
+    NeighborListCompressor(void* buffer, std::size_t maxSize)
+        : buffer_(reinterpret_cast<std::uint8_t*>(buffer))
+        , maxNibbles_(2 * maxSize)
+    {
+    }
+
+    bool add(std::uint32_t nbIndex)
+    {
+        const std::uint32_t value = nbIndex - prevNbIndex_;
+        prevNbIndex_              = nbIndex;
+
+        if (value == 1)
+        {
+            if (ones_) { writeNibble(nNibbles_ - 1, readNibble(nNibbles_ - 1) + 1); }
+            else
+            {
+                if (!pushBackNibble(9)) return false;
+            }
+            assert(readNibble(nNibbles_ - 1) == ones_ + 9);
+            if (++ones_ >= 7) ones_ = 0;
+        }
+        else
+        {
+            const int n = std::max((std::bit_width(value) + 3) / 4, 1);
+            if (!pushBackNibble(n)) return false;
+            for (int i = 0; i < n; ++i)
+                if (!pushBackNibble((value >> (4 * i)) & 0xf)) return false;
+            ones_ = 0;
+        }
+        ++nNeighbors_;
+        return true;
+    }
+
+    void decompress(std::uint32_t* buffer, std::size_t maxNeighbors) const
+    {
+        std::size_t i          = 0;
+        std::uint32_t previous = 0;
+        while (i < nNibbles_)
+        {
+            const int n = readNibble(i++);
+            if (n > 8)
+            {
+                for (int j = 0; j < n - 8; ++j)
+                {
+                    if (!maxNeighbors--) return;
+                    *(buffer++) = ++previous;
+                }
+            }
+            else
+            {
+                std::uint32_t value = 0;
+                for (int j = 0; j < n; ++j)
+                {
+                    if (i >= nNibbles_) return;
+                    const std::uint32_t d = readNibble(i++);
+                    value |= d << (4 * j);
+                }
+                previous += value;
+                if (!maxNeighbors--) return;
+                *(buffer++) = previous;
+            }
+        }
+    }
+
+    std::size_t size() const { return nNeighbors_; }
+    std::size_t nbytes() const { return (nNibbles_ + 1) / 2; }
+
+private:
+    void writeNibble(std::size_t index, std::uint8_t value)
+    {
+        assert(index < nNibbles_);
+        assert(nNibbles_ <= maxNibbles_);
+        assert(value < 16);
+        const auto [byte, offset] = std::div(index, 2);
+        const std::uint8_t mask   = offset ? 0x0f : 0xf0;
+        buffer_[byte]             = (buffer_[byte] & mask) | (value << (4 * offset));
+    }
+
+    std::uint8_t readNibble(std::size_t index) const
+    {
+        assert(index < nNibbles_);
+        assert(nNibbles_ <= maxNibbles_);
+        const auto [byte, offset] = std::div(index, 2);
+        return (buffer_[byte] >> (4 * offset)) & 0xf;
+    }
+
+    bool pushBackNibble(std::uint8_t value)
+    {
+        if (nNibbles_ >= maxNibbles_) return false;
+        writeNibble(nNibbles_++, value);
+        return true;
+    }
+
+    std::uint8_t* buffer_;
+    std::size_t maxNibbles_, nNibbles_ = 0, nNeighbors_ = 0, ones_ = 0;
+    std::uint32_t prevNbIndex_ = 0;
+};
+
+} // namespace cstone
---
+++ test/unit/neighbors/compressneighbors.cpp
@@ -0,0 +1,78 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 CSCS, ETH Zurich
+ *               2021 University of Basel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*! @file
+ * @brief Neighbor list compression tests
+ *
+ * @author Felix Thaler <thaler@cscs.ch>
+ */
+
+#include "gtest/gtest.h"
+#include "cstone/compressneighbors.hpp"
+
+using namespace cstone;
+
+TEST(CompressNeighbors, roundtrip)
+{
+    std::array<std::uint32_t, 17> nbs = {300, 301, 302, 100, 101, 200, 400, 402, 403,
+                                         404, 405, 406, 407, 408, 409, 410, 411};
+
+    char buffer[4 * nbs.size()];
+    NeighborListCompressor comp(buffer, sizeof(buffer));
+
+    for (auto nb : nbs)
+        EXPECT_TRUE(comp.add(nb));
+
+    EXPECT_EQ(comp.size(), nbs.size());
+    EXPECT_LT(comp.nbytes(), 4 * nbs.size());
+
+    std::array<std::uint32_t, nbs.size()> nbs_roundtrip;
+    comp.decompress(nbs_roundtrip.data(), nbs_roundtrip.size());
+
+    for (std::size_t i = 0; i < nbs.size(); ++i)
+        EXPECT_EQ(nbs[i], nbs_roundtrip[i]);
+}
+
+TEST(CompressNeighbors, smallBuffer)
+{
+    std::array<std::uint32_t, 4> nbs = {300, 301, 302, 100};
+
+    char buffer[5];
+    NeighborListCompressor comp(buffer, sizeof(buffer));
+
+    EXPECT_TRUE(comp.add(nbs[0]));
+    EXPECT_TRUE(comp.add(nbs[1]));
+    EXPECT_TRUE(comp.add(nbs[2]));
+    EXPECT_FALSE(comp.add(nbs[3]));
+
+    EXPECT_EQ(comp.size(), 3);
+    EXPECT_LE(comp.nbytes(), 5);
+
+    std::array<std::uint32_t, nbs.size()> nbs_roundtrip;
+    comp.decompress(nbs_roundtrip.data(), nbs_roundtrip.size());
+
+    for (std::size_t i = 0; i < comp.size(); ++i)
+        EXPECT_EQ(nbs[i], nbs_roundtrip[i]);
+}
