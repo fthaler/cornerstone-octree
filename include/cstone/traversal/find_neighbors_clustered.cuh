86df79484befcef63e50d539b715751a2fa52c66
Felix Thaler <thaler@cscs.ch>
2024-10-08 15:40:11 +0200

    Revert "Use cooperative_groups::reduce"
    This reverts commit f0f86d059829ace97d5d5a1257d489887662ebf7.

--- include/cstone/traversal/find_neighbors_clustered.cuh
+++ include/cstone/traversal/find_neighbors_clustered.cuh
@@ -36,7 +36,6 @@
 #include <cuda/barrier>
 #include <cuda/pipeline>
 #include <cooperative_groups.h>
-#include <cooperative_groups/reduce.h>
 #include <cub/warp/warp_merge_sort.cuh>
 
 #include "cstone/compressneighbors.hpp"
@@ -680,8 +679,9 @@ __global__ __launch_bounds__(ClusterConfig::iSize* ClusterConfig::jSize* warpsPe
             }
         }
 
-        detail::tuple_foreach([&](auto& sum) { sum = cg::reduce(warp, sum, cg::plus<std::decay_t<decltype(sum)>>()); },
-                              sums);
+#pragma unroll
+        for (unsigned offset = GpuConfig::warpSize / 2; offset >= ClusterConfig::iSize; offset /= 2)
+            detail::tuple_foreach([&](auto& sum) { sum += warp.shfl_down(sum, offset); }, sums);
 
         if (block.thread_index().y == 0 & i < lastBody)
             detail::tuple_foreach([i](auto& res, auto const& sum) { res[i] = sum; }, std::make_tuple(results...), sums);
